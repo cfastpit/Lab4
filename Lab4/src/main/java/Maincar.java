d77fd68931a4146a0cf20a36e32b8eb81e0f6e6f
0778430 <0778430@ACIS-TUTOR01-P.pittstate.edu>
2020-03-13 09:35:17 -0500

    car is done

--- Lab4/src/main/java/Maincar.java
+++ Lab4/src/main/java/Maincar.java
@@ -2,7 +2,7 @@
 public class Maincar {
 
     public static void main(String[] args) {
-        Car mycar = new Car(ford,2000, 1);
+        Car mycar = new Car("ford",2000, 1);
         System.out.println("car is accelerating  for the first time at "+mycar.getAccelerate());
         System.out.println("car is accelerating for the second time at "+mycar.getAccelerate());
         System.out.println("car is accelerating for the third time at "+mycar.getAccelerate());
